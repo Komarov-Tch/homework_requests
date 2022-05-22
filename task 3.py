887a7f440a84eeba34dca06afc1b6ae24c1b4c35
Komarov-Tch <75240295+Komarov-Tch@users.noreply.github.com>
2022-05-22 15:28:00 +0700

    Хм. Непонятный результат

--- task 3.py
+++ task 3.py
@@ -1,19 +1,27 @@
 import requests
 from pprint import pprint
 import datetime as dt
-
+import time
 
 class StackOvewrflow:
     def tag_request(self, tag):
         date = f'{dt.date.today() - dt.timedelta(days=2)}'
-        url = f'https://api.stackexchange.com/2.2/questions?fromdate={date}' \
-              f'&order=desc&sort=activity&tagged={tag}&site=stackoverflow'
-        response = requests.get(url)
-        result = [(i['title'], i['link']) for i in response.json()['items']]
-        if response:
-            pprint(result)
-        else:
-            print('Ошибка выполнения запроса: \n' + url + f'\nHttp статус: {response.status_code}, ({response.reason})')
+        result = []
+        for i in range(1, 1000):
+            result_temp = []
+            url = f'https://api.stackexchange.com/2.3/questions?page={i}&fromdate={date}' \
+                  f'&order=desc&sort=creation&tagged={tag}&site=stackoverflow'
+            response = requests.get(url)
+            if response:
+                result_temp = [i['link'] for i in response.json()['items']]
+                result.extend(result_temp)
+            else:
+                print(f'Http статус: {response.status_code}, ({response.reason})')
+                break
+            time.sleep(1)
+        print(len(result))
+        print(i)
+        pprint(result)
 
 
 if __name__ == '__main__':
