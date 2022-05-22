0804247f21ad717f1c8de5bda4d438677cfb6cb1
Komarov-Tch <75240295+Komarov-Tch@users.noreply.github.com>
2022-05-22 11:08:51 +0700

    Задание 3 не совсем готово

---
+++ task 3.py
@@ -0,0 +1,21 @@
+import requests
+from pprint import pprint
+import datetime as dt
+
+
+class StackOvewrflow:
+    def tag_request(self, tag):
+        date = f'{dt.date.today() - dt.timedelta(days=2)}'
+        url = f'https://api.stackexchange.com/2.2/questions?fromdate={date}' \
+              f'&order=desc&sort=activity&tagged={tag}&site=stackoverflow'
+        response = requests.get(url)
+        result = [i['title'] for i in response.json()['items']]
+        if response:
+            pprint(result)
+        else:
+            print('Ошибка выполнения запроса: \n' + url + f'\nHttp статус: {response.status_code}, ({response.reason})')
+
+
+if __name__ == '__main__':
+    so = StackOvewrflow()
+    so.tag_request('Python')
