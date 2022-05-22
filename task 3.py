0c4b94d1647f81b5e37fb143bd6a9dcab6b67608
Komarov-Tch <75240295+Komarov-Tch@users.noreply.github.com>
2022-05-22 17:16:25 +0700

    После долгих измененений ограничил поиск, если пошли пустые страницы.

--- task 3.py
+++ task 3.py
@@ -13,9 +13,13 @@ class StackOvewrflow:
             url = f'https://api.stackexchange.com/2.3/questions?page={i}&pagesize=100&fromdate={date}' \
                   f'&order=desc&sort=creation&tagged={tag}&site=stackoverflow'
             response = requests.get(url)
-            if response:
-                result_temp = [i['link'] for i in response.json()['items']]
+            response_json = response.json()
+            if response and response_json['items']:
+                result_temp = [i['link'] for i in response_json['items']]
                 result.extend(result_temp)
+            elif response and bool(response_json['items']) is False:
+                print('Поиск завершен')
+                break
             else:
                 print(f'Http статус: {response.status_code}, ({response.reason})')
                 break
