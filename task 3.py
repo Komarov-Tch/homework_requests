c7eec480144a085a661fb2b46c99cac82550ccb1
Komarov-Tch <75240295+Komarov-Tch@users.noreply.github.com>
2022-05-22 16:27:29 +0700

    Изменил количество страниц для вызова

--- task 3.py
+++ task 3.py
@@ -3,13 +3,14 @@ from pprint import pprint
 import datetime as dt
 import time
 
+
 class StackOvewrflow:
     def tag_request(self, tag):
         date = f'{dt.date.today() - dt.timedelta(days=2)}'
         result = []
         for i in range(1, 1000):
             result_temp = []
-            url = f'https://api.stackexchange.com/2.3/questions?page={i}&fromdate={date}' \
+            url = f'https://api.stackexchange.com/2.3/questions?page={i}&pagesize=100&fromdate={date}' \
                   f'&order=desc&sort=creation&tagged={tag}&site=stackoverflow'
             response = requests.get(url)
             if response:
