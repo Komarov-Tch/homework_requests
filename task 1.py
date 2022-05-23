6cb1772f40cdfc8c8a082197290e61540700436b
Komarov-Tch <komarovegr@gmail.com>
2022-05-23 15:23:29 +0700

    Добавил проверку на героев тёзок

--- task 1.py
+++ task 1.py
@@ -2,11 +2,15 @@ import requests
 
 
 def the_smartest(adress='https://superheroapi.com/api/2619421814940190/search/',
-                 heroes=['Hulk', 'Captain America', 'Thanos']):
-    result = []
+                 heroes=['Hulk', 'Captain America', 'Thanos'],
+                 stat='intelligence'):
     for hero_name in heroes:
+        result = []
         response = requests.get(adress + hero_name)
-        result.append((hero_name, response.json()['results'][0]['powerstats']['intelligence']))
+        result = [(hero_name, character['powerstats'][stat])
+                  for character in response.json()['results']
+                  if character['name'] == hero_name]
+
     return max(result, key=lambda x: int(x[1]))
 
 
